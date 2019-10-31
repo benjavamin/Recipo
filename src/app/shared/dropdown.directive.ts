b1109fc8aa1561b3e3ffdebb4b4354fffcc6852c
Benjamin Bajic <benjamin.bajic@pragmatio.com>
2019-10-31 14:17:57 +0100

    dropdowns and directives

---
+++ src/app/shared/dropdown.directive.ts
@@ -0,0 +1,17 @@
+import {Directive, ElementRef, HostBinding, HostListener, Input, OnInit, Renderer2} from '@angular/core';
+
+@Directive({
+  selector: '[appDropdown]'
+})
+export class DropdownDirective {
+  @HostBinding('class.open') isOpen = false;
+
+  @HostListener('document: click', ['$event']) toggleDropDown(eventData: Event) {
+    this.isOpen = this.elRef.nativeElement.contains(event.target) ? !this.isOpen : false;
+  }
+
+
+  constructor(private elRef: ElementRef) {
+
+  }
+}
